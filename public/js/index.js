7da4473a95e047c29f391fd719a4d7f18d1df48a
Brad Pridgeon <tpridge@clemson.edu>
2018-06-24 20:22:20 -0400

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
 	console.log('Connected to server.');
-
-	socket.emit('createMessage', {
-		from: 'Andrew',
-		text: 'test message'
-	});
 });
 
 socket.on('disconnect', function () {		
